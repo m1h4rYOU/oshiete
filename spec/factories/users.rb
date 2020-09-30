1bc53ec655a9cbc6711fc59036aa5cb82cb42555
miharu0626 <miharu.pearls@gmail.com>
2020-09-30 23:45:03 +0900

    userモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
+    nickname { Faker::FunnyName.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 8) }
+    password_confirmation { password }
     
   end
 end
