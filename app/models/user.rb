2ad18803ea47117a76c4fe64bcebf0debfb03273
miharu0626 <miharu.pearls@gmail.com>
2020-09-30 22:47:18 +0900

    ビューファイルの修正、resources追記およびバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  with_options presence: true, length: { minimum: 6 } do
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :password_confirmation, format: { with: VALID_PASSWORD_REGEX }
+  end
+      
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_many :comments
+  has_many :recipes
 end
