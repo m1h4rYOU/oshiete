60a58e5db6b5bb2b7ff35d0a678266af214a770a
miharu0626 <miharu.pearls@gmail.com>
2020-10-08 00:50:30 +0900

    モデルの単体テスト

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :recipe do
+    dish_name { 'あいうえお' }
+    genre_id { Faker::Number.between(from: 2, to: 4) }
+    ingredient { 'あいうえお' }
+    detail { 'あいうえお' }
+  end
+end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  describe '#create' do
+    before do
+      @recipe = FactoryBot.build(:item)
+      @recipe.image = fixture_file_upload('/public/images/test_image.png')
+    end
+
+    it 'imageが存在すれば商品出品できること' do
+      expect(@recipe).to be_valid
+    end
+
+    it 'imageが空では登録できないこと' do
+      @recipe.image = nil
+      @recipe.valid?
+      expect(@recipe.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'dish_nameが空では登録できないこと' do
+      @recipe.dish_name = nil
+      @recipe.valid?
+      expect(@recipe.errors.full_messages).to include("Dish_name can't be blank")
+    end
+
+    it 'genre_idが「--」では登録できないこと' do
+      @recipe.genre_id = 1
+      @recipe.valid?
+      expect(@recipe.errors.full_messages).to include('Genre_id must be other than 1')
+    end
+
+    it 'ingredientが空では登録できないこと' do
+      @recipe.detail = nil
+      @recipe.valid?
+      expect(@recipe.errors.full_messages).to include("Ingredient can't be blank")
+    end
+
+    it 'detailが空では登録できないこと' do
+      @recipe.detail = nil
+      @recipe.valid?
+      expect(@recipe.errors.full_messages).to include("Detail can't be blank")
+    end
+  end
+end
