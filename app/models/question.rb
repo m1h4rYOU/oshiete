d1ab79f68475410c9a65fc4d1ced9eaa12364c55
miharu0626 <miharu.pearls@gmail.com>
2020-10-15 18:50:23 +0900

    モデルの編集

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,9 @@
 class Question < ApplicationRecord
+  belongs_to :user
+  belongs_to :recipe
+  + belongs_to :parent,  class_name: "Question", optional: true
+  + has_many   :reply, class_name: "Question", foreign_key: :parent_id, dependent: :destroy
+
+  validates :content, presence: true, length: { in: 1..1000 }
+
 end
