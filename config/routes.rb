a6df69dddd5b59cf89404795d845b44dd34a9385
miharu0626 <miharu.pearls@gmail.com>
2020-10-15 18:53:49 +0900

    ルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
   resources :users, only: [:new, :create, :edit, :update]
-  resources :recipes
+  resources :recipes do
+    resources :questions
+  end
 
 end
