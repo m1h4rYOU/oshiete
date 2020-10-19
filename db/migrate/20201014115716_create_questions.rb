4f9aa893f5cee10a63c892820ddc05ae4f403900
miharu0626 <miharu.pearls@gmail.com>
2020-10-19 17:56:45 +0900

    questionsテーブル修正

--- db/migrate/20201014115716_create_questions.rb
+++ db/migrate/20201014115716_create_questions.rb
@@ -3,8 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
     create_table :questions do |t|
       t.references :user,    null: false, foreign_key: true
       t.references :recipe,  null: false, foreign_key: true
-      t.references :parent,  null: false, foreign_key: { to_table: :questions }
-      t.text :content, null: false
+      t.text :text, null: false
       t.timestamps
     end
   end
