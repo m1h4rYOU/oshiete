c80e295b08cef979c684e7c6510b3191510a5bfd
miharu0626 <miharu.pearls@gmail.com>
2020-10-19 17:56:14 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
   resources :users, only: [:new, :create, :edit, :update]
-  resources :recipes do
-    resources :questions
-  end
+  resources :recipes
+  resources :questions
 
 end
