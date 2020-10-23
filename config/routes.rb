8ed7f0cf3c8357fae71ce9d009dc3d8e60dd55e1
miharu0626 <miharu.pearls@gmail.com>
2020-10-23 20:16:06 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
+  }
   root to: 'recipes#index'
   resources :users, only: [:new, :create, :edit, :update]
   resources :recipes
   resources :questions
 
+end
+
+devise_scope :user do
+  post 'users/guest_sign_in', to: 'users/sessions#new_guest'
 end
