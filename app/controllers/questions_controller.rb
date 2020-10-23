968e92fa19f0b6a9f8bb522eba7301802384ec52
miharu0626 <miharu.pearls@gmail.com>
2020-10-23 23:30:39 +0900

    レシピ毎に質問を振り分け修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,11 @@
 class QuestionsController < ApplicationController
   def new
-    @questions = Question.all
+    @questions = Question.where(recipe_id: params[:recipe_id])
     @question = Question.new
   end
 
   def create
-    @question = Question.new(text: params[:question][:text],recipe_id: Recipe.last.id)
+    @question = Question.new(text: params[:question][:text],recipe_id: params[:recipe_id], user_id: current_user.id)
     if @question.save
       ActionCable.server.broadcast 'question_channel', content: @question
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
   resources :users, only: [:new, :create, :edit, :update]
-  resources :recipes
-  resources :questions
-
+  resources :recipes do
+    resources :questions
+  end
 end
 
