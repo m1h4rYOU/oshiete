2613dd559d61ad897fe4acbd50494190f30786f7
miharu0626 <miharu.pearls@gmail.com>
2020-10-25 20:50:19 +0900

    recipesのingredientのデータ型を修正

--- db/migrate/20201001052206_create_recipes.rb
+++ db/migrate/20201001052206_create_recipes.rb
@@ -4,7 +4,7 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
       t.references :user,  null: false,foreign_key: true
       t.string :dish_name, null: false
       t.integer :genre_id, null: false
-      t.string :ingredient,null: false
+      t.text :ingredient,  null: false
       t.text :detail,      null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_115716) do
+ActiveRecord::Schema.define(version: 2020_10_24_065659) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_115716) do
     t.bigint "user_id", null: false
     t.string "dish_name", null: false
     t.integer "genre_id", null: false
-    t.string "ingredient", null: false
+    t.text "ingredient", null: false
     t.text "detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
