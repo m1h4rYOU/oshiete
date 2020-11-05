3210fec6788d347fc7cdaf0e78cc64ec7e646433
miharu0626 <miharu.pearls@gmail.com>
2020-11-05 21:44:45 +0900

    Basic認証取外し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
 
   def new_guest
     user = User.find_or_create_by!(email: 'guest@email.com') do |user|
@@ -18,11 +17,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :teacher])
   end
 
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
 end
